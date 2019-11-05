fe5fb43bd2b2263df1ff76e22034a8ecbc908080
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-05 11:51:54 -0500

    create stub postsReducer, wire to index.js, refactor fetchPosts, incorporate redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
+import jsonPlaceHolder from '../apis/jsonPlaceHolder'
+
 export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS"
+  return async function(dispatch, getState) {
+    const response = await jsonPlaceHolder.get('/posts')
+
+    dispatch({ type: "FETCH_POSTS", payload: response})
   }
 }
