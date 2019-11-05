9b3be74b081a4d0e8227c70f6f6cba53cf1ee276
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-05 14:48:13 -0500

    add logic to postsReducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,18 @@
-export default () => {
-  return 123
+export default (state=[], action) => {
+  switch (action.type) {
+      case 'FETCH_POSTS':
+        return action.payload
+      default:
+        return state
+  }
 }
+
+// Above code w/out switch statement (kinda obvious...)
+
+// export default (state=[], action) => {}
+//   if (action.type === "FETCH_POSTS") {
+//     return action.payload
+//   }
+//
+//   return state
+// }
