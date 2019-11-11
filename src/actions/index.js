77ae8f646ff7e81747ced1eceb7fa1ebc9a42ef1
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-11 10:32:30 -0500

    add/comment out alternate chain syntax for fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,13 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 
   const userIds = _.uniq(_.map(getState().posts, 'userId'))
   userIds.forEach(id => dispatch(fetchUser(id)))
+
+  // optional refactor / lodash chain syntax
+  // _.chain(getState().props)
+  // .map('userId')
+  // .uniq()
+  // .forEach(id => dispatch(fetchUser(id)))
+  // .value()
 }
 
 export const fetchPosts = () => {
