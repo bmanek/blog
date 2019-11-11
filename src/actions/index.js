01ba2e5b474e29a0093931beac009a69a870db3b
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-11 10:24:03 -0500

    add fetchPostsAndUsers action creator, solve overfetching issue gracefully, remove redundant fetchUser call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder'
 import _ from 'lodash'
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts())
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  userIds.forEach(id => dispatch(fetchUser(id)))
+}
+
 export const fetchPosts = () => {
   return async function(dispatch, getState) {
     const response = await jsonPlaceHolder.get('/posts')
@@ -10,13 +17,21 @@ export const fetchPosts = () => {
   }
 }
 
-// syntax below is the naive implementation of memoization
-export const fetchUser = (id) => dispatch => {
-  _fetchUser(id, dispatch);
-}
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceHolder.get(`/users/${id}`)
 
   dispatch({ type: "FETCH_USER", payload: response.data })
-})
+}
+
+
+// syntax below is the naive implementation of memoization
+// export const fetchUser = (id) => dispatch => {
+//   _fetchUser(id, dispatch);
+// }
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`)
+//
+//   dispatch({ type: "FETCH_USER", payload: response.data })
+// })
